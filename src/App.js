eedc8b371643e58bdd3c649cec2133f279785c0d
TeylerL <46732911+TaylerL@users.noreply.github.com>
2021-02-17 01:25:00 -0500

    Setting up Redux

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -14,14 +19,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <Fragment>
-      <SearchBar />
-      <div className='container'>
-        <AddBtn />
-        <AddLogModal />
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <SearchBar />
+        <div className='container'>
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
